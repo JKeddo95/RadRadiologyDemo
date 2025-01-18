f785e1573efc9affb09286d16d82628754e6bfc7
jkeddo_ <jjkeddo@gmail.com>
2025-01-18 02:59:02 -0800

    DEV-01--WIP-SVG-Generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import happyPatient from "/happyPatient.svg";
+import hearts from "/hearts.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://react.dev" target="_blank">
+          <img src={happyPatient} className="react" alt="Rad Radiology logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={hearts} className="react" alt="Rad Radiology hearts" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Rad Radiology</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          We help our providers <code>provide</code>!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
